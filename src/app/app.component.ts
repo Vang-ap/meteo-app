10b715f38a6811cd84ef55ea16b0de055ee2872f
Plia Vang <pliaannevang.02@gmail.com>
2021-08-20 15:36:12 +0200

    token retrieval and storage in localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private loginService: LoginService
+  ) {
+
+  }
+
   title = 'meteo-app';
+
+  ngOnInit() {
+    console.log(this.loginService.isUserLogged());
+  }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Login } from 'src/models/login';
 
@@ -8,15 +9,37 @@ import { Login } from 'src/models/login';
   providedIn: 'root'
 })
 export class LoginService {
+  private token!: string;
 
   constructor(
     private httpClient: HttpClient,
-  ) { }
+  ) {
+    const token = localStorage.getItem('token');
+    this.token = token || '';
+  }
 
-  loginOfUser(userInfoLogin: Login): Observable<Login> {
+  getToken() {
+    return this.token;
+  }
+
+  isUserLogged() {
+    return !!this.token;
+  }
+
+  loginOfUser(userInfoLogin: Login): Observable<{ access_token: string }> {
     const endpointUrl = `${environment.apiUrl}/auth/login`;
     console.log(userInfoLogin);
 
-    return this.httpClient.post<Login>(endpointUrl, userInfoLogin);
+    return this.httpClient
+      .post<{ access_token: string }>(endpointUrl, userInfoLogin)
+      .pipe(
+        map(response => {
+          this.token = response.access_token;
+          localStorage.setItem('token', this.token);
+
+          return response;
+        })
+      );
   }
+
 }
