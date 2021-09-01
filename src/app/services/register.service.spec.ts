5432bef79342e2ea72e80a45d6e7a26b44030a30
Plia Vang <pliaannevang.02@gmail.com>
2021-09-01 16:22:35 +0200

    test for register service

--- src/app/services/register.service.spec.ts
+++ src/app/services/register.service.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { RegisterService } from './register.service';
 
 describe('RegisterService', () => {
   let service: RegisterService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(RegisterService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should sends user info', () => {
+    const userInfo = {
+      username: 'test name',
+      email: 'test@mail.com',
+      password: 'test password',
+      age: 30,
+      city: 'test ville',
+    }
+
+    spyOn(service, 'sendsUserInfo');
+
+    service.sendsUserInfo(userInfo);
+
+    expect(service.sendsUserInfo).toHaveBeenCalledWith(userInfo);
+
   });
 });
